589b0a42eeb66dc092443c9a553e0b06d10e53ea
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-05-05 00:30:12 +0000

    modified README

--- lib/horoscope/scraper.rb
+++ lib/horoscope/scraper.rb
@@ -2,7 +2,12 @@ class Horoscope::Scraper
   
   def self.scrape_zodiac
     doc = Nokogiri::HTML(open("https://www.elle.com/horoscopes/daily/"))
-    binding.pry
+    zodiac = doc.css("div.simple-item-title.item-title")
+    
+    zodiac.each do |text|
+      output = text.text.split(" ")
+      Horoscope::Zodiac.new(output[0])
+    end
   end
   
 end
