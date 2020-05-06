b1039d8bb4454b79ac24fa832680d1199db6a087
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-05-06 19:26:09 +0000

    updates

--- lib/horoscope/cli.rb
+++ lib/horoscope/cli.rb
@@ -3,7 +3,9 @@ class Horoscope::CLI
   def call
     puts "\nWonder what the stars have aligned for you today? 
     Find out in this Daily Horoscope App!\n"
-    get_zodiac
+    
+    @zodiac = Horoscope::Zodiac.all
+    
     list_zodiac
     
     input = gets.strip.downcase
@@ -13,21 +15,18 @@ class Horoscope::CLI
           sign = @zodiac[input.to_i - 1]
           puts "Today's Horoscope for #{sign.sign}:"
         elsif input == "list"
+          puts "\nChoose your Zodiac Sign by selecting its index:\n"
           list_zodiac
         else puts "Wrong input! Try again!"
           input = gets.strip.downcase
         end
-        puts "Thank you for swinging by! See you tomorrow!"
       end
       
-  end
+      puts "Thank you for swinging by! See you tomorrow!"  
   
-  def get_zodiac
-    @zodiac = Horoscope::Zodiac.all
   end
 
   def list_zodiac
-    puts "\nChoose your Zodiac Sign by selecting its index:\n"
     @zodiac.each.with_index(1) {|zodiac, index| puts "#{index}. #{zodiac.sign}"}
   end
 
